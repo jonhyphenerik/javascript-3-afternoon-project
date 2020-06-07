62b0069b49da07b812c261e6d64faa7373a784ef
Jon-Erik Nissen <jenissen@me.com>
2020-06-07 09:20:44 -0700

    did work on higher order

--- higher-order.js
+++ higher-order.js
@@ -63,7 +63,7 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation  = populations.reduce(runningTotal, curElement => runningTotal+curElement)
 
 
 
@@ -89,7 +89,7 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter(monster => monster.CP > 200);
 
 
 
@@ -106,7 +106,7 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
+let orderTotals = orders.map(order => order.price* (1+order.tax))
 
 
 
@@ -126,6 +126,8 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
-
-
+let bobsTotal = purchases.reduce(runningTotal, curElement => {
+  if(curElement[owner]==="Bob"){
+    runningTotal + curElement[price]
+  }
+});
